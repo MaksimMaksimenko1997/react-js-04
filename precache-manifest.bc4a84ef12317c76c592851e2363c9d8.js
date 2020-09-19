acce4de00944699b8b61f81e60554900218e6668
MaksimMaksimenko1997 <59928413+MaksimMaksimenko1997@users.noreply.github.com>
2020-09-19 10:16:44 +0300

    Updates

---
+++ precache-manifest.bc4a84ef12317c76c592851e2363c9d8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c394b00d0fdd8658667b0eafeadcd3c7",
+    "url": "/react-js-04/index.html"
+  },
+  {
+    "revision": "2d1477a246ec4f4626bb",
+    "url": "/react-js-04/static/css/home-page.4388a1d9.chunk.css"
+  },
+  {
+    "revision": "f81bcc7e8cb8d7e3ac6c",
+    "url": "/react-js-04/static/css/main.dcdf1ebd.chunk.css"
+  },
+  {
+    "revision": "afa160796275f272d33d",
+    "url": "/react-js-04/static/css/movies-details-page.ebc78b0a.chunk.css"
+  },
+  {
+    "revision": "bc8b4e70cf0569c1a9d0",
+    "url": "/react-js-04/static/css/movies-page.00eb4e4c.chunk.css"
+  },
+  {
+    "revision": "a61d73f97feaa5cb5645",
+    "url": "/react-js-04/static/js/5.e7b3e834.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-js-04/static/js/5.e7b3e834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1477a246ec4f4626bb",
+    "url": "/react-js-04/static/js/home-page.c6c263db.chunk.js"
+  },
+  {
+    "revision": "f81bcc7e8cb8d7e3ac6c",
+    "url": "/react-js-04/static/js/main.980f5a26.chunk.js"
+  },
+  {
+    "revision": "afa160796275f272d33d",
+    "url": "/react-js-04/static/js/movies-details-page.1edd61d4.chunk.js"
+  },
+  {
+    "revision": "bc8b4e70cf0569c1a9d0",
+    "url": "/react-js-04/static/js/movies-page.21e1c755.chunk.js"
+  },
+  {
+    "revision": "7467173e8cd792afe529",
+    "url": "/react-js-04/static/js/runtime-main.a9b3e8b6.js"
+  }
+]);
